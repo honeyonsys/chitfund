f997cc597170e0565838d6c194d1e8bc97e49327
honeyonsys@gmail.com <honeyonsys@gmail.com>
2024-03-19 16:12:04 +0530

    added graph layout

--- dashboard.php
+++ dashboard.php
@@ -29,17 +29,27 @@
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-12">
+                        <div class="col-md-6">
                             <div class="card">
                                 <div class="card-header">Overall Progress</div>
-                            </div>
-                            <div class="card-body">
-                            <div>
-                                <canvas id="myChart"></canvas>
-                            </div>
-
+                            
+                                <div class="card-body">
+                                  <div class="canvas-wrapper">
+                                      <canvas id="myChart"></canvas>
+                                  </div>
+                                </div>
                             </div>
                         </div>  
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-header">Payment Status</div>
+                                <div class="card-body">
+                                    <div class="canvas-wrapper">
+                                        <canvas class="chart" id="barchart"></canvas>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -48,11 +58,10 @@
     <?php include('includes/includedjs.php');?>
     <script>
   const ctx = document.getElementById('myChart');
-
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      labels: ['Group1', 'Group2', 'Group3', 'Group4', 'Group5', 'Group6'],
       datasets: [{
         label: '# of Votes',
         data: [12, 19, 3, 5, 2, 3],
@@ -67,6 +76,46 @@
       }
     }
   });
+
+
+  var chart2 = document.getElementById('barchart');
+  var myChart2 = new Chart(chart2, {
+    type: 'bar',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+                label: 'Paid',
+                backgroundColor: "rgba(54, 162, 235, 0.5)",
+                borderColor: "rgb(54, 162, 235)",
+                borderWidth: 1,
+                data: ["20", "30", "40", "50", "60", "70", "80"],
+            }, {
+                label: 'Unpaid',
+                backgroundColor: "rgba(244, 67, 54, 0.5)",
+                borderColor: "rgb(255, 99, 132)",
+                borderWidth: 1,
+                data: ["5", "15", "25", "35", "45", "35", "25"],
+        }]
+    },
+    options: {
+        animation: {
+            duration: 2000,
+            easing: 'easeOutQuart',
+        },
+        plugins: {
+            legend: {
+                display: true,
+                position: 'top',
+            },
+            title: {
+                display: true,
+                text: 'Revenue',
+                position: 'left',
+            },
+        },
+    }
+    });
+
 </script>
 </body>
 
